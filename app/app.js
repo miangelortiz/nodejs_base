f3b2faf15eecbb6bbc5ac6f6b8785778ee9f5ff5
miangelortiz <maortizolid@gmail.com>
2021-10-20 15:03:10 +0200

    add comment

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 require('./utils/import_env').import()
 const logger = require('./utils/logger')
 
-logger.info('DAQ ENERGY ALARMS ' + (require('../package.json')).version)
-logger.info('Starting monitoring...')
+logger.info('NODEJS BASE ' + (require('../package.json')).version)
+logger.info('Starting watching...')
 
 function intervalFunc () {
   logger.debug('Cant stop me now!')
